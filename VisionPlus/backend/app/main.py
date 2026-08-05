8df892e75c4ba31f108c57895087ca4eb801a342
Guna M <gunamadhaiyan2101@gmail.com>
2026-08-06 01:40:08 +0530

    Fix CORS for Vercel

--- VisionPlus/backend/app/main.py
+++ VisionPlus/backend/app/main.py
@@ -60,6 +60,9 @@ app.add_middleware(
         "http://127.0.0.1:4173",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+
+        # Vercel frontend
+        "https://safe-vision-git-main-guna6.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
